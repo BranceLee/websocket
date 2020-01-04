696ba9dc1339a389a95b8fbe628cc848fadfac60
BranceLee <475815059@qq.com>
2020-01-04 18:45:25 +0800

    websocket init

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/extensions
+import App from './App.jsx';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<App />, document.getElementById('root'));
